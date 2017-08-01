583eedb12b5af01d353c38f1764a1409a9d8937e
Kevin Hellemun <khellemun@bunq.com>
2017-08-01 18:32:29 +0200

    Add unit-tests

---
+++ Tests/BunqSdkCsharpTest/Model/Generated/PaymentTests.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections.Generic;
+using Bunq.Sdk.Context;
+using Bunq.Sdk.Model.Generated;
+using Bunq.Sdk.Model.Generated.Object;
+using Xunit;
+
+namespace Bunq.Sdk.Tests.Model.Generated
+{
+    /// <summary>
+    /// Tests:
+    ///     Payment
+    /// </summary>
+    public class PaymentTests
+    {
+        /// <summary>
+        /// Config values.
+        /// </summary>
+        private const string AMOUNT_IN_EUR = "00.01";
+        private const string FIELD_CURRENCY = "EUR";
+        private const string FIELD_PAYMENT_DESCRIPTION = "C# test Payment";
+
+        private static readonly int USER_ID = Config.GetUserId();
+        private static readonly int MONETARY_ACCOUNT_ID = Config.GetMonetarytAccountId();
+        private static readonly Pointer COUNTER_PARTY_SELF = Config.GetCounterAliasSelf();
+        private static readonly Pointer COUNTER_PARTY_OTHER = Config.GetCounterAliasOther();
+
+        /// <summary>
+        /// API context to use for the test API calls.
+        /// </summary>
+        private static readonly ApiContext API_CONTEXT = ApiContextHandler.GetApiContext();
+        
+        /// <summary>
+        /// Tests making a payment to another sanndbox user.
+        /// 
+        /// This test has no asserion as it is testing to see if the code runs without errors.
+        /// </summary>
+        [Fact]
+        public void TestMakePaymentToOtherUser()
+        {
+            var requestMap = new Dictionary<string, object>
+            {
+                {Payment.FIELD_AMOUNT, new Amount(AMOUNT_IN_EUR, FIELD_CURRENCY)},
+                {Payment.FIELD_DESCRIPTION, FIELD_PAYMENT_DESCRIPTION},
+                {Payment.FIELD_COUNTERPARTY_ALIAS, COUNTER_PARTY_OTHER}
+            };
+
+            Payment.Create(API_CONTEXT, requestMap, USER_ID, MONETARY_ACCOUNT_ID);
+        }
+
+        /// <summary>
+        /// Tests making a payment to another monetary account.
+        /// 
+        /// This test has no asserion as it is testing to see if the code runs without errors.
+        /// </summary>
+        [Fact]
+        public void TestMakePaymentToOtherAccount()
+        {
+            var requestMap = new Dictionary<string, object>
+            {
+                {Payment.FIELD_AMOUNT, new Amount(AMOUNT_IN_EUR, FIELD_CURRENCY)},
+                {Payment.FIELD_DESCRIPTION, FIELD_PAYMENT_DESCRIPTION},
+                {Payment.FIELD_COUNTERPARTY_ALIAS, COUNTER_PARTY_SELF}
+            };
+
+            Payment.Create(API_CONTEXT, requestMap, USER_ID, MONETARY_ACCOUNT_ID);
+        }
+    }
+}
