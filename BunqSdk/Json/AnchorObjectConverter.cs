e4e36d43aa684adb18b36aa3616e8c889b030520
Kevin Hellemun <khellemun@bunq.com>
2017-12-21 13:56:12 +0100

    Fixed indentation and added missing new line. (sdk_csharp#43)

--- BunqSdk/Json/AnchorObjectConverter.cs
+++ BunqSdk/Json/AnchorObjectConverter.cs
@@ -13,27 +13,27 @@ namespace Bunq.Sdk.Json
 
         public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer)
         {
-            
             new JsonSerializer().Serialize(writer, value);
-            
         }
 
         public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
         {
             var jsonSerializer = JsonSerializer.CreateDefault(
                 new JsonSerializerSettings
-            {
-                ContractResolver = new BunqContractResolver(new List<Type> {typeof(IAnchorObjectInterface)}),
-                DateFormatString = FORMAT_DATE,
-                FloatParseHandling = FloatParseHandling.Decimal,
-                Formatting = Formatting.Indented,
-                NullValueHandling = NullValueHandling.Ignore,
-            });
+                {
+                    ContractResolver = new BunqContractResolver(new List<Type> {typeof(IAnchorObjectInterface)}),
+                    DateFormatString = FORMAT_DATE,
+                    FloatParseHandling = FloatParseHandling.Decimal,
+                    Formatting = Formatting.Indented,
+                    NullValueHandling = NullValueHandling.Ignore,
+                }
+            );
             
             JToken token = JObject.Load(reader);
             var model = (IAnchorObjectInterface) jsonSerializer.Deserialize(token.CreateReader(), objectType);
 
             if (!model.IsAllFieldNull()) return model;
+            
             var fields = objectType.GetProperties();
 
             foreach (var field in fields)
