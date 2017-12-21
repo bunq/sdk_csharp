43d9903c85496eab1e79ab4b854b8106662f26a3
Kevin Hellemun <khellemun@bunq.com>
2017-12-21 14:43:23 +0100

    Flipped IsAssignableFrom statement. (bunq/sdk_csharp#43)

--- BunqSdk/Json/AnchorObjectConverter.cs
+++ BunqSdk/Json/AnchorObjectConverter.cs
@@ -55,7 +55,7 @@ namespace Bunq.Sdk.Json
 
         public override bool CanConvert(Type objectType)
         {
-            return objectType.IsAssignableFrom(typeof(IAnchorObjectInterface));
+            return typeof(IAnchorObjectInterface).IsAssignableFrom(objectType);
         }
     }
 }
