8bafb67a33b9de1be824809483924125749c1c73
Delano REIJNIERSE <delano.reijnierse@zrefinery.nl>
2019-04-15 09:13:51 +0200

    Added endpoint for Events \(bunq/sdk_csharp#121\)

---
+++ BunqSdk/Model/Generated/Endpoint/Event.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections.Generic;
+using Bunq.Sdk.Http;
+using Bunq.Sdk.Model.Core;
+using Bunq.Sdk.Model.Generated.Object;
+using Newtonsoft.Json;
+
+namespace Bunq.Sdk.Model.Generated.Endpoint
+{
+    public class Event : BunqModel
+    {
+        /// <summary>
+        /// Endpoint constants.
+        /// </summary>
+        protected const string ENDPOINT_URL_LISTING = "user/{0}/event";
+
+        protected const string ENDPOINT_URL_READ = "user/{0}/event/{1}";
+
+        /// <summary>
+        /// Object type.
+        /// </summary>
+        private const string OBJECT_TYPE_GET = "Event";
+
+        /// <summary>
+        /// The id of the event object.
+        /// </summary>
+        [JsonProperty(PropertyName = "id")]
+        public int? Id { get; set; }
+
+        /// <summary>
+        /// The timestamp of the event object's creation.
+        /// </summary>
+        [JsonProperty(PropertyName = "created")]
+        public string Created { get; set; }
+
+        /// <summary>
+        /// The timestamp of the event object's last update.
+        /// </summary>
+        [JsonProperty(PropertyName = "updated")]
+        public string Updated { get; set; }
+
+        /// <summary>
+        /// The timestamp of the event object's last update.
+        /// </summary>
+        [JsonProperty(PropertyName = "action")]
+        public string Action { get; set; }
+
+        /// <summary>
+        /// The timestamp of the event object's last update.
+        /// </summary>
+        [JsonProperty(PropertyName = "user_id")]
+        public int? UserId { get; set; }
+
+        /// <summary>
+        /// The id of the MonetaryAccount the Payment was made to or from (depending on whether this is an incoming or
+        /// outgoing Payment).
+        /// </summary>
+        [JsonProperty(PropertyName = "monetary_account_id")]
+        public int? MonetaryAccountId { get; set; }
+
+        /// <summary>
+        /// The object of the event
+        /// </summary>
+        [JsonProperty(PropertyName = "object")]
+        public EventObject Object { get; set; }
+
+        /// <summary>
+        /// The timestamp of the event object's last update.
+        /// </summary>
+        [JsonProperty(PropertyName = "status")]
+        public string Status { get; set; }
+
+
+        /// <summary>
+        /// Get a collection of events.
+        /// You can add query the parameters monetary_account_id, status and/or display_user_event to filter the response. When monetary_account_id={id,id} is provided only events that relate to these monetary account ids are returned.
+        /// When status={AWAITING_REPLY/FINALIZED} is provided the response only contains events with the status AWAITING_REPLY or FINALIZED.
+        /// When display_user_event={true/false} is set to false user events are excluded from the response, when not provided user events are displayed.
+        /// User events are events that are not related to a monetary account (for example: connect invites).
+        /// </summary>
+        public static BunqResponse<List<Event>> List(IDictionary<string, string> urlParams = null,
+            IDictionary<string, string> customHeaders = null)
+        {
+            if (urlParams == null) urlParams = new Dictionary<string, string>();
+            if (customHeaders == null) customHeaders = new Dictionary<string, string>();
+
+            var apiClient = new ApiClient(GetApiContext());
+            var responseRaw = apiClient.Get(string.Format(ENDPOINT_URL_LISTING, DetermineUserId()), urlParams,
+                customHeaders);
+
+            return FromJsonList<Event>(responseRaw, OBJECT_TYPE_GET);
+        }
+
+        /// <summary>
+        /// </summary>
+        public static BunqResponse<Event> Get(int eventId, IDictionary<string, string> customHeaders = null)
+        {
+            if (customHeaders == null) customHeaders = new Dictionary<string, string>();
+
+            var apiClient = new ApiClient(GetApiContext());
+            var responseRaw = apiClient.Get(string.Format(ENDPOINT_URL_READ, DetermineUserId(), eventId),
+                new Dictionary<string, string>(), customHeaders);
+
+            return FromJson<Event>(responseRaw, OBJECT_TYPE_GET);
+        }
+
+        public override bool IsAllFieldNull()
+        {
+            if (Action != null)
+                return false;
+
+            if (UserId != null)
+                return false;
+
+            if (MonetaryAccountId != null)
+                return false;
+
+            if (Object != null)
+                return false;
+
+            if (Status != null)
+                return false;
+
+            return true;
+        }
+
+        /// <summary>
+        /// </summary>
+        public static Event CreateFromJsonString(string json)
+        {
+            return BunqModel.CreateFromJsonString<Event>(json);
+        }
+    }
+}
---
+++ BunqSdk/Model/Generated/Object/EventObject.cs
@@ -0,0 +1,191 @@
+﻿using Bunq.Sdk.Model.Core;
+using Bunq.Sdk.Model.Generated.Endpoint;
+using Newtonsoft.Json;
+
+namespace Bunq.Sdk.Model.Generated.Object
+{
+    public class EventObject : BunqModel
+    {
+        /// <summary>
+
+        /// The BunqMeTab of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "BunqMeTab")]
+        public BunqMeTab BunqMeTab { get; set; }
+
+        /// <summary>
+        /// The BunqMeTabResultResponse of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "BunqMeTabResultResponse")]
+        public BunqMeTabResultResponse BunqMeTabResultResponse { get; set; }
+
+        /// <summary>
+        /// The BunqMeFundraiserResult of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "BunqMeFundraiserResult")]
+        public BunqMeFundraiserResult BunqMeFundraiserResult { get; set; }
+
+        /// <summary>
+        /// The Card of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "Card")]
+        public Card Card { get; set; }
+
+        /// <summary>
+        /// The CardDebit of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "CardDebit")]
+        public CardDebit CardDebit { get; set; }
+
+        /// <summary>
+        /// The DraftPayment of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "DraftPayment")]
+        public DraftPayment DraftPayment { get; set; }
+
+        ///// <summary>
+        ///// The FeatureAnnouncement of the event's object
+        ///// </summary>
+        //[JsonProperty(PropertyName = "FeatureAnnouncement")]
+        //public FeatureAnnouncement FeatureAnnouncement { get; set; }
+
+        /// <summary>
+        /// The IdealMerchantTransaction of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "IdealMerchantTransaction")]
+        public IdealMerchantTransaction IdealMerchantTransaction { get; set; }
+
+        /// <summary>
+        /// The Invoice of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "Invoice")]
+        public Invoice Invoice { get; set; }
+
+        /// <summary>
+        /// The ScheduledPayment of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "ScheduledPayment")]
+        public SchedulePayment ScheduledPayment { get; set; }
+
+        /// <summary>
+        /// The ScheduledPaymentBatch of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "ScheduledPaymentBatch")]
+        public SchedulePaymentBatch ScheduledPaymentBatch { get; set; }
+
+        /// <summary>
+        /// The ScheduledInstance of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "ScheduledInstance")]
+        public ScheduleInstance ScheduledInstance { get; set; }
+
+        /// <summary>
+        /// The MasterCardAction of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "MasterCardAction")]
+        public MasterCardAction MasterCardAction { get; set; }
+
+        ///// <summary>
+        ///// The BankSwitchServiceNetherlandsIncomingPayment of the event's object
+        ///// </summary>
+        //[JsonProperty(PropertyName = "BankSwitchServiceNetherlandsIncomingPayment")]
+        //public BankSwitchServiceNetherlandsIncomingPayment BankSwitchServiceNetherlandsIncomingPayment { get; set; }
+
+        /// <summary>
+        /// The Payment of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "Payment")]
+        public Payment Payment { get; set; }
+
+        /// <summary>
+        /// The PaymentBatch of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "PaymentBatch")]
+        public PaymentBatch PaymentBatch { get; set; }
+
+        /// <summary>
+        /// The RequestInquiryBatch of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "RequestInquiryBatch")]
+        public RequestInquiryBatch RequestInquiryBatch { get; set; }
+
+        /// <summary>
+        /// The RequestInquiry of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "RequestInquiry")]
+        public RequestInquiry RequestInquiry { get; set; }
+
+        /// <summary>
+        /// The RequestResponse of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "RequestResponse")]
+        public RequestResponse RequestResponse { get; set; }
+
+        ///// <summary>
+        ///// The RewardRecipient of the event's object
+        ///// </summary>
+        //[JsonProperty(PropertyName = "RewardRecipient")]
+        //public RewardRecipient RewardRecipient { get; set; }
+
+        ///// <summary>
+        ///// The RewardSender of the event's object
+        ///// </summary>
+        //[JsonProperty(PropertyName = "RewardSender")]
+        //public RewardSender RewardSender { get; set; }
+
+        ///// <summary>
+        ///// The ShareInviteBankInquiryBatch of the event's object
+        ///// </summary>
+        //[JsonProperty(PropertyName = "ShareInviteBankInquiryBatch")]
+        //public ShareInviteBankInquiryBatch ShareInviteBankInquiryBatch { get; set; }
+
+        /// <summary>
+        /// The ShareInviteBankInquiry of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "ShareInviteBankInquiry")]
+        public ShareInviteBankInquiry ShareInviteBankInquiry { get; set; }
+
+        /// <summary>
+        /// The ShareInviteBankResponse of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "ShareInviteBankResponse")]
+        public ShareInviteBankResponse ShareInviteBankResponse { get; set; }
+
+        ///// <summary>
+        ///// The SofortMerchantTransaction of the event's object
+        ///// </summary>
+        //[JsonProperty(PropertyName = "SofortMerchantTransaction")]
+        //public SofortMerchantTransaction SofortMerchantTransaction { get; set; }
+
+        /// <summary>
+        /// The TabResultInquiry of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "TabResultInquiry")]
+        public TabResultInquiry TabResultInquiry { get; set; }
+
+        /// <summary>
+        /// The TabResultResponse of the event's object
+        /// </summary>
+        [JsonProperty(PropertyName = "TabResultResponse")]
+        public TabResultResponse TabResultResponse { get; set; }
+
+        ///// <summary>
+        ///// The TransferwiseTransfer of the event's object
+        ///// </summary>
+        //[JsonProperty(PropertyName = "TransferwiseTransfer")]
+        //public TransferwiseTransfer TransferwiseTransfer { get; set; }
+
+
+        /// <summary>
+        /// </summary>
+        public static Event CreateFromJsonString(string json)
+        {
+            return BunqModel.CreateFromJsonString<Event>(json);
+        }
+
+        public override bool IsAllFieldNull()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
